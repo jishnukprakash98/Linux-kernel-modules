113a793c20e939e88ae27906f47c8ac5f8a30dd2
Akhileswar V <aakkhhil79@gmail.com>
2019-03-14 11:23:16 +0530

    Add files via upload
    Program to list all processes in the OS.

---
+++ processes.c
@@ -0,0 +1,20 @@
+
+    #include < linux/kernel.h >
+    #include < linux/sched.h >
+    #include < linux/module.h >
+
+    int init_module(void)
+    {
+    	struct task_struct *task;
+    	for_each_process(task)
+    	{
+    		printk("%s [%d]\n",task->comm , task->pid);
+    	}
+
+    	return 0;
+    }
+
+    void cleanup_module(void)
+    {
+    	printk(KERN_INFO "Cleaning Up.\n");
+    }
