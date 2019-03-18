8e7e797497ef0b448409971248db4b8ef23467fe
Akhil Gokuldas <47827849+AkhilDasPanicker@users.noreply.github.com>
2019-03-18 10:26:43 +0530

    First version

---
+++ pf.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+ 
+int main()
+{
+
+	//char *buf = malloc(10*sizeof(char));
+	char buf[20] = "kill -STOP";
+	int p;
+	printf("\nresult:%d\n",system("ps all"));      
+	
+	printf("Enter the desired pid:");
+	scanf("%d",&p);
+	//char pid[5];
+	//itoa(p,pid,10);
+	//buf="kill -STOP";                                                          
+	//strcat(buf,pid);
+	//snprintf (buf, sizeof(buf), "%d",n)
+        //printf("Please enter a number:\n");                                        
+        //scanf("%d",&a);                                                            
+       // printf("Your number is: %d\n",a);                                          
+        //snprintf(buf,5, "%d",p);
+        sprintf(buf,"kill -STOP %d",p);                                   
+        printf("\nresult:%d",system(buf));      
+       
+	return 0;
+
+}
+ 
