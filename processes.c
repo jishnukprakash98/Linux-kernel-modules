23567081fb61187a81839ae610b9e127408262e6
Akhileswar V <aakkhhil79@gmail.com>
2019-03-29 13:53:06 +0530

    Update processes.c

--- processes.c
+++ processes.c
@@ -1,21 +1,31 @@
-    /* Akhileswar */
-    
-    #include < linux/kernel.h >
-    #include < linux/sched.h >
-    #include < linux/module.h >
+    #include <linux/module.h>       // Needed by all modules
+#include <linux/kernel.h>       // KERN_INFO
+#include <linux/sched.h>        // for_each_process, pr_info
+#include <linux/sched/signal.h> 
 
-    int init_module(void)
-    {
-    	struct task_struct *task;
-    	for_each_process(task)
-    	{
-    		printk("%s [%d]\n",task->comm , task->pid);
-    	}
+MODULE_LICENSE("GPL");
 
-    	return 0;
-    }
+void procs_info_print(void)
+{
+        struct task_struct* task_list;
+        size_t process_counter = 0;
+        for_each_process(task_list) {
+                pr_info("== %s [%d]\n", task_list->comm, task_list->pid);
+                ++process_counter;
+        }
+        printk(KERN_INFO "== Number of process: %zu\n", process_counter);
+}
 
-    void cleanup_module(void)
-    {
-    	printk(KERN_INFO "Cleaning Up.\n");
-    }
+int init_module(void)
+{
+        printk(KERN_INFO "[ INIT ==\n");
+
+        procs_info_print();
+
+        return 0;
+}
+
+void cleanup_module(void)
+{
+        printk(KERN_INFO "== CLEANUP ]\n");
+}
