48ece3b826888a2b8e2943e5c819d49c8ed5ebfc
Jishnu <31334731+jishnukprakash98@users.noreply.github.com>
2019-04-09 14:33:46 +0530

    Update get_activewindow.sh
    added user input section

--- get_activewindow.sh
+++ get_activewindow.sh
@@ -1,4 +1,7 @@
-for x in $(seq 1 5); do sleep 5; wmctrl -lp | grep $(xprop -root | \
+echo "enter the limit"
+read limit
+echo "Entered limit is : $limit"
+for x in $(seq 1 $limit); do sleep 5; wmctrl -lp | grep $(xprop -root | \
     grep _NET_ACTIVE_WINDOW | head -1 | awk '{print $5}' | sed 's/,//' | \
     sed 's/^0x/0x0/'); done >file2
 wmctrl -lp >file1
