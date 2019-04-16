06cb6d79ea1f17016ca4093258f8fb7a084b3ff5
akshaybabu3575 <31334617+akshaybabu3575@users.noreply.github.com>
2019-04-16 11:59:19 +0530

    print kernel stack

--- kernelstack.c
+++ kernelstack.c
@@ -6,14 +6,17 @@ int main(int argc, char *argv[])
 {
 	FILE *file = NULL;
 	int pid=atoi(argv[1]);
-	char path_buffer[100],c;
+	int i=0;
+	char path_buffer[100],c[25],stor[100][25];
    	sprintf(path_buffer,"/proc/%d/stack",pid);
    	file = fopen(path_buffer,"r");
-	c = fgetc(file); 
-   	while (c != EOF) 
+	fgets(c,25,file); 
+   	while (!feof(file)) 
    	{ 
-        	printf ("%c", c); 
-        	c = fgetc(file); 
+		sprintf(stor[i],"%s",c);
+		printf("%s",stor[i]);		
+		i++;         	
+		fgets(c,25,file); 
     	} 
   
    	fclose(file);
