5f3df4a89a8a3df21bec0f09eb7ca7feb359640e
Akhileswar V <aakkhhil79@gmail.com>
2019-04-20 19:23:52 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,19 +1,17 @@
 #include<stdlib.h>
 #include <time.h> 
+
 int main(){
-int secs=60;
-int unit_time=10;
-int steps=secs/unit_time;
-printf("Watching CPU usage... ");
-for((i=0;i<steps;i++))
-{
-system( "ps -eocomm,pcpu | tail -n +2 >> /tmp/cpu_usage.$$");
-sleep(unit_time);
-printf("CPU Eaters");
-system(cat /tmp/cpu_usage.$$ | \);
+	int secs=60;
+	int unit_time=10;
+	int steps=secs/unit_time;
 
-
-
-
-return 0;
+	printf("Watching CPU usage... ");
+	for((i=0;i<steps;i++)) {
+		system( "ps -eocomm,pcpu | tail -n +2 >> /tmp/cpu_usage.$$");
+		sleep(unit_time);
+	}
+	printf("CPU Eaters");
+	system(cat /tmp/cpu_usage.$$ | \);
+	return 0;
 }
