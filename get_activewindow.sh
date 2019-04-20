9472a3b29633269a545d1120a67fb674126c986c
Jishnu <31334731+jishnukprakash98@users.noreply.github.com>
2019-04-20 14:29:30 +0530

    Update get_activewindow.sh
    first working version

--- get_activewindow.sh
+++ get_activewindow.sh
@@ -1,16 +1,19 @@
 echo "enter the limit"
 read limit
-echo "Entered limit is : $limit"
 for x in $(seq 1 $limit); do sleep 5; wmctrl -lp | grep $(xprop -root | \
     grep _NET_ACTIVE_WINDOW | head -1 | awk '{print $5}' | sed 's/,//' | \
     sed 's/^0x/0x0/'); done >file2
 wmctrl -lp >file1
-echo "you should close this window~ Yes/No?"
-read option
-if [ "$option" = "y" ]; then
-               echo "expression evaluated as true"
-fi
-awk -F' ' 'NR==FNR{c[$3]++;next};c[$3] <= 0' file2 file1
-echo "enter the pid of window to be closed"
-read pid
-wmctrl -i -c $pid
+
+awk -F' ' 'NR==FNR{c[$3]++;next};c[$3] <= 0' file2 file1 >file3
+
+while read line ; do
+    set $line
+    zenity --question --text="Do you wish to close this window?: $5"
+    option=$?
+    echo "$option"
+    if [ "$option" = "0" ]; then
+               wmctrl -i -c $1
+    fi
+done <file3
+
