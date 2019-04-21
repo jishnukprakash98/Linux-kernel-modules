2b9fa2fa37f828ccf30fdffa53f85ae9bfcf11f3
Akhileswar V <aakkhhil79@gmail.com>
2019-04-21 14:06:23 +0530

    Update get_activewindow.sh

--- get_activewindow.sh
+++ get_activewindow.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for x in {1..10} ; do
+for x in $(seq 1 10) ; do
 	sleep 5; wmctrl -lp | grep $(xprop -root | \
 	grep _NET_ACTIVE_WINDOW | head -1 | awk '{print $5}' | sed 's/,//' | \
 	sed 's/^0x/0x0/');
