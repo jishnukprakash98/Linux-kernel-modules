8c6be4dfce355faf3bcf78ad9a5c2bbf45358b5d
Jishnu <31334731+jishnukprakash98@users.noreply.github.com>
2019-04-30 10:58:21 +0530

    Update get_activewindow.sh
    printing full name

--- get_activewindow.sh
+++ get_activewindow.sh
@@ -9,11 +9,11 @@ awk -F' ' 'NR==FNR{c[$3]++;next};c[$3] <= 0' file2 file1 >file3
 
 while read line ; do
     set $line
-    zenity --question --text="Do you wish to close this window?: $5"
+    name=$(echo $line|cut -d" " -f5-)
+    zenity --question --text="Do you wish to close this window?: $name"
     option=$?
     echo "$option"
     if [ "$option" = "0" ]; then
                wmctrl -i -c $1
     fi
 done <file3
-
