e99206119c2e04d1712f362b02a5c39658db4d2e
akshaybabu3575 <31334617+akshaybabu3575@users.noreply.github.com>
2019-05-01 22:17:10 +0530

    Update npkernel.c

--- npkernel.c
+++ npkernel.c
@@ -4,6 +4,7 @@
 #include <linux/sched/signal.h> 
 #include <linux/types.h> 
 #include <linux/string.h>
+#include <linux/timekeeping32.h>
 
 MODULE_LICENSE("GPL");
 
@@ -20,30 +21,34 @@ void procs_info_print(void)
 {
         struct task_struct* task_list;
         size_t process_counter = 0; 
-        int i=0; 
-        int totproc; 
-        u64 inittime; 
+        //int i=0; 
+        //int totproc; 
            
-        for_each_process(task_list) {
-        
-        if (process_counter==0){
-        	inittime=task_list->real_start_time;
-        }   
-        
-        	pr_info("== name:%s\tpid:[%d]\tstarttime:[%llu]\tmemory:[%llu]\n", task_list->comm, 						task_list->pid,task_list->real_start_time-inittime, (task_list->acct_rss_mem1+task_list->acct_vm_mem1)/40000000);
+        for_each_process(task_list) {       
+        	pr_info("== name:%s\n\t\t\tpid:[%d]\t\tstarttime:[%llu],[%llu]\t\tmemory:[%llu]\n", task_list->comm,task_list->pid,task_list->start_time/100,task_list->utime+task_list->stime ,task_list->acct_rss_mem1);
 
-        strcpy(proc[i].name,task_list->comm);
+        /*strcpy(proc[i].name,task_list->comm);
         proc[i].pid=task_list->pid;
         proc[i].strtime=task_list->real_start_time;
         proc[i].memsize=task_list->acct_rss_mem1;
-        	++i;
+        	++i;*/
                 ++process_counter;
         }
 
-        totproc=i;
+        //totproc=i;
+        
+        struct timespec ts_start;
+        getnstimeofday(&ts_start);
+        
         printk(KERN_INFO "== Number of process: %zu\n", process_counter);
-}
+        printk(KERN_INFO "== Time: %lu\n", ts_start);
+    
+     }
+/*
+        if((task_list->start_time/100)>ts_start && task_list->acct_rss_mem1>30000000000000)
+            pr_info("name:%s\t\tpid:[%d]",task_list->comm,task_list->pid);
 
+*/
 int init_module(void)
 {
         printk(KERN_INFO "[ INIT ==\n");
