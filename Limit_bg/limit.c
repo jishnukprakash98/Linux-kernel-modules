c6cc3a2dda7c4a79031fd641c89ea691e0489413
Akhileswar V <aakkhhil79@gmail.com>
2019-05-15 13:34:30 +0530

    Modified comments
    Removed unnecessary print statements and comments

--- Limit_bg/limit.c
+++ Limit_bg/limit.c
@@ -16,12 +16,13 @@ MODULE_AUTHOR("Akhileswar");
 
 static struct task_struct *limit_thread;
 
-struct mystruct {
+struct proc_data{
      int data ;
      struct task_struct *process;
      struct list_head mylist ;
 };
 
+/* Initialise proc_head as the head of the linked list*/
 LIST_HEAD(proc_head) ;
 
 /*The PID of the shell in which we wnat to limit background processes is taken as input parameter*/ 
@@ -31,7 +32,10 @@ static int limit = 0;
 module_param(shell, int, S_IRUGO);
 module_param(limit, int, S_IRUGO);
 
-/* Return true if task is a kernel process or init.*/
+/*	
+	Input : Pointer to task_struct of a task/process.
+	Returns true if task is a kernel process or init.
+*/
 static int is_kernel_proc(struct task_struct *task)
 {
 	if (task->parent) {
@@ -45,6 +49,7 @@ static int is_kernel_proc(struct task_struct *task)
 		return 1;
 }
 
+/* This function is spawned as a thread. Takes void pointer a parameter. */
 int thread_fn(void *a)
 {
 	static char path_buffer[25];
@@ -53,31 +58,31 @@ int thread_fn(void *a)
 	static struct task_struct *task;
 	static struct file *fil;
 	static pid_t  pgrp, session_id;
-	static int size, i = 0, j = 0, tpgid = 0, count = 0;
+	static int size, i = 0, j = 0, tpgid = 0, count = 0, c = 0;
 	static bool in_array = false;
 	static mm_segment_t fs;
 	
-	struct mystruct *temp = NULL;
+	struct proc_data*temp = NULL;
+	struct proc_data*cursor = NULL;
+	struct proc_data*tm = NULL;
 
 	printk("Shell ID: %d\n", shell);
-
-	//array = kmalloc(limit * sizeof(int), GFP_KERNEL);
 
 	allow_signal(SIGKILL);
 
 	while (!kthread_should_stop()) {
+		
 		count = 0;
+
 		for_each_process(task) {
-			if(is_kernel_proc(task)) {
-				//printk(KERN_INFO "Kernel Process: %d [%s]",
-				//	task->pid, task->comm);
-			} else {
+
+			if(!is_kernel_proc(task)) {
+				
 				pgrp = pid_vnr(task_pgrp(task));
 				session_id = pid_vnr(task_session(task));
 				state = task_state_to_char(task);
 
 				sprintf(path_buffer,"/proc/%d/stat", task->pid);
-
 
 				fil = filp_open(path_buffer, O_RDONLY, 0);
 
@@ -125,51 +130,43 @@ int thread_fn(void *a)
 				/*Filtering background processes and sending QUIT signal*/
 				if(session_id == shell && state != 'T' && pgrp != tpgid && task->pid != shell) {
 					
-					//INIT_LIST_HEAD(&temp->mylist);
-					
-					//printk(KERN_INFO "Adding %d", temp->data);
-					//list_add(&temp->mylist,&proc_head);
-					
-					if(list_empty(&proc_head) == 0){
+					if(list_empty(&proc_head) == 0) {
                                         
-						struct mystruct *cur;
+						struct proc_data *cur;
 						list_for_each_entry(cur, &proc_head, mylist) {
 				            		 
-				            	if(cur->data == task->pid)
-				            		in_array = true;			
-				            	}
-				    	}   	
+				            if(cur->data == task->pid)
+				            	in_array = true;			
+				        }
+				    }   	
 			        if(in_array == false) {
-			        	temp = kmalloc(sizeof(struct mystruct), GFP_KERNEL);
+			        	temp = kmalloc(sizeof(struct proc_data), GFP_KERNEL);
 						temp->data = task->pid;
 						temp->process = task;
-			        		//printk(KERN_INFO "Adding %d", temp->data);
+			        		
 						list_add(&temp->mylist,&proc_head);
 						count++;	
 					}
 					
 				} 		
-			}
+			} 
+			
 		}
 		
 
         if(count > limit) {
-        	int c = 0;
+        	c = 0;
         	list_for_each_entry(temp, &proc_head, mylist) {
         		c++;
         		
-        		if(c > limit){
+        		if(c > limit) {
         			force_sig(SIGTERM, temp->process);
         			printk(KERN_INFO "Killed %s %d since limit = %d",temp->process->comm, temp->data, limit);
-        			
-        			//list_del(&temp->mylist);
-            			//kfree(temp);
         		}
         	}
 
         }
 
-        struct mystruct *cursor, *tm;
         /*Deleting the linked list*/
         list_for_each_entry_safe(cursor, tm, &proc_head, mylist) {
             list_del(&cursor->mylist);
@@ -208,6 +205,17 @@ static void __exit stop_program(void)
 module_init(start_program);
 module_exit(stop_program);
 
+static void __exit stop_program(void)
+{
 
+	if (limit_thread) {
+		int ret = kthread_stop(limit_thread);
 
+ 		if(ret == 0)
+  			printk(KERN_INFO "Thread stopped");
+	}
 
+}
+
+module_init(start_program);
+module_exit(stop_program);
